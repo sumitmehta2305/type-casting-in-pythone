50b3ed32207f1b59eb08322e0da62ca470c2dbad
sumitmehta2305 <mehtasumit1432@gmail.com>
2024-08-10 19:07:27 +0530

    type casting example

---
+++ task1.py
@@ -0,0 +1,40 @@
+# # # x= 10
+# # # y= float(x)
+# # # print(y)
+
+# # # x= 15 
+# # # y= str(x)
+# # # print(y)
+
+# # # x=13
+# # # y= complex(x)
+# # # print (y)
+
+# # # x=15
+# # # y=bool(x)
+# # # print(y)
+
+# # x=13.6
+# # y= int(x)
+# # print (y)
+ 
+# x=13.7
+# y=bool(x)
+# print (y)
+
+# x=13.7
+# y=complex(x)
+# print (y)
+  
+# x=13.7
+# y=b(x)
+# print (y)
+
+# x=13.7
+# y=str(x)
+# print (y)
+
+
+
+
+
